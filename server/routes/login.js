a759de62b7cdb541d114eb3d3cab06d7ec77093d
Elliot Thomas <elliotnthomas@gmail.com>
2017-01-23 09:50:53 -0600

    changes

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,14 +6,4 @@ router.post('/', passport.authenticate('local'), function(req, res) {
   res.sendStatus(200);
 });
 
-router.get('/test', function(req, res) {
-  console.log('hit test route');
-  console.log('req.user', req.user);
-  console.log('req.session', req.session);
-
-  console.log('is authed?', req.isAuthenticated());
-
-  res.sendStatus(200);
-});
-
 module.exports = router;
